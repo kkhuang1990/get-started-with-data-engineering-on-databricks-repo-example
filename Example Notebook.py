0081117555109d309cd6903fa6aab6a1398a46dc
kkhuang1990@gmail.com <kkhuang1990@gmail.com>
2023-11-29 06:43:28 +0000

    add one line to the last

--- Example Notebook.py
+++ Example Notebook.py
@@ -9,6 +9,4 @@ data = [[1, "Elia"], [2, "Teo"], [3, "Fang"]]
 sdf = spark.createDataFrame(data, schema="id LONG, name STRING")
 display(sdf)
 
-# COMMAND ----------
-
-
+print("I have added one line into this example notebook")
